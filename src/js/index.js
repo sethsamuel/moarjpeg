84b76313408ecb6872f83e4887bb718a21ff12cd
Seth Samuel <sethfsamuel@gmail.com>
2015-12-14 22:30:26 -0500

    Regnerate on slide

--- src/js/index.js
+++ src/js/index.js
@@ -5,33 +5,43 @@ const context = canvas.getContext("2d");
 context.fillStyle = "rgb(255,255,255)";
 context.fillRect(0,0,canvas.width,canvas.height);
 
-let quality = 10;
-
 const blair = new Image();
 blair.addEventListener("load", () => {
 	context.drawImage(blair, 0, 0, canvas.width, canvas.height);
 });
 blair.src = "images/blair.jpg";
 
-document.getElementById("generate-canvas").addEventListener("click", () => {
+const generateCanvas = (quality) => {
 	const timerId = "Generate";
 	console.time(timerId);
 	const url = canvas.toDataURL("image/jpeg", quality/100);
 	const img = document.getElementById("get-img").src = url;
 	console.timeEnd(timerId);
-});
+}
 
-document.getElementById("generate-jpeg").addEventListener("click", () => {
+const generateJpeg = (quality) => {
 	const timerId = "JPEG";
 	console.time(timerId);
 	const data = context.getImageData(0,0,canvas.width,canvas.height);
 	const jpegImageData = jpeg.encode(data, quality);
 	const img = document.getElementById("jpg-img").src = 'data:image/jpeg;base64,' + jpegImageData.data.toString("base64");
 	console.timeEnd(timerId);
+}
+
+document.getElementById("generate-canvas").addEventListener("click", generateCanvas);
+
+document.getElementById("generate-jpeg").addEventListener("click", generateJpeg);
+
+document.getElementById("generate-opengl").addEventListener("click", () => {
+	const timerId = "OpenGL";
+	console.time(timerId);
+	console.timeEnd(timerId);
 });
 
-document.getElementById("quality").addEventListener("change", () => {
-	quality = document.getElementById("quality").value;
+document.getElementById("quality").addEventListener("input", () => {
+	const quality = document.getElementById("quality").value;
+	generateCanvas(quality);
+	generateJpeg(quality);
 
 });
 
