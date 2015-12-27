f0c554b3af5b2cc77187f4b2c536debba318a4f7
Seth Samuel <sethfsamuel@gmail.com>
2015-12-27 15:49:10 -0500

    Server

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,41 @@ var jade = require("gulp-jade");
 var sass = require("gulp-sass");
 var webpack = require("gulp-webpack");
 var named = require("vinyl-named");
+var babel = require("gulp-babel");
 
 gulp.task("views", function(){
 	gulp.src("src/*.jade")
 		.pipe(jade({
 			pretty: true
 		}).on("error", console.error))
-		.pipe(gulp.dest("dist"));
+		.pipe(gulp.dest("dist/public"));
 });
 
 gulp.task("scripts", function(){
-	gulp.src(["src/js/index.js","src/js/jpeg-worker.js"])
+	gulp.src(["src/js/client/index.js","src/js/client/jpeg-worker.js"])
 		.pipe(named())
 		.pipe(webpack(require("./webpack.config.js")))
-		.pipe(gulp.dest("dist/js"));
+		.pipe(gulp.dest("dist/public/js"));
+});
+
+gulp.task("server", function(){
+	gulp.src(["src/js/index.js"])
+		.pipe(babel())
+		.pipe(gulp.dest("dist"));
 });
 
 gulp.task("styles", function(){
 	gulp.src("src/sass/*")
 		.pipe(sass().on("error", sass.logError))
-		.pipe(gulp.dest("dist/css"));
+		.pipe(gulp.dest("dist/public/css"));
 });
 
 gulp.task("images", function(){
 	gulp.src("src/images/*")
-		.pipe(gulp.dest("dist/images"));
+		.pipe(gulp.dest("dist/public/images"));
 });
 
-gulp.task("build", ["views", "scripts", "styles", "images"]);
+gulp.task("build", ["views", "scripts", "server", "styles", "images"]);
 
 gulp.task("watch", function(){
 	gulp.watch("src/**/*", ["build"]);
---
+++ src/js/client/index.js
@@ -0,0 +1,63 @@
+const worker = new Worker("js/jpeg-worker.js");
+
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+context.fillStyle = "rgb(255,255,255)";
+context.fillRect(0,0,canvas.width,canvas.height);
+
+const blair = new Image();
+blair.addEventListener("load", () => {
+	context.drawImage(blair, 0, 0, canvas.width, canvas.height);
+});
+blair.src = "images/blair.jpg";
+
+const generateCanvas = (quality) => {
+	const timerId = "Generate";
+	console.time(timerId);
+	const tmpImage = new Image();
+	const tmpCanvas = document.getElementById("tmp-canvas")
+	const tmpContext = tmpCanvas.getContext("2d");
+	let dataUrl;
+	let iterationCount = 1000;
+	do{
+		dataUrl = canvas.toDataURL("image/jpeg", quality/100);
+		tmpImage.src = dataUrl;
+		tmpContext.drawImage(tmpImage, 0, 0, tmpCanvas.width, tmpCanvas.height);
+		dataUrl = tmpCanvas.toDataURL("image/jpeg", quality/100);
+	}while(iterationCount--)
+	const img = document.getElementById("get-img").src = dataUrl;
+	console.timeEnd(timerId);
+}
+
+const generateJpeg = (quality) => {
+	return;
+	const timerId = "JPEG";
+	console.time(timerId);
+	const data = context.getImageData(0,0,canvas.width,canvas.height);
+	worker.postMessage({quality: quality, width: data.width, height: data.height, data: data.data.buffer}, [data.data.buffer]);
+	worker.onmessage = (e) => {
+		document.getElementById("jpg-img").src = 'data:image/jpeg;base64,' + e.data.url;
+		console.timeEnd(timerId);
+	}
+}
+
+document.getElementById("generate-canvas").addEventListener("click", () => generateCanvas(document.getElementById("quality").value));
+
+document.getElementById("generate-jpeg").addEventListener("click", () => generateJpeg(document.getElementById("quality").value));
+
+document.getElementById("generate-opengl").addEventListener("click", () => {
+	const timerId = "OpenGL";
+	console.time(timerId);
+	console.timeEnd(timerId);
+});
+
+document.getElementById("quality").addEventListener("input", () => {
+	const quality = document.getElementById("quality").value;
+	generateCanvas(quality);
+	generateJpeg(quality);
+
+});
+
+document.getElementById("quality").value = quality;
+
+console.log("MOAR", canvas);
